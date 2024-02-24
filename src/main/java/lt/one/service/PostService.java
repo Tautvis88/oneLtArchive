9429cf5fec336615f0492ad2bc83a4fe84d86e66
Tautvydas Pagalys <tpagalys@gmail.com>
2024-02-24 06:47:57 +0200

    Second commit of my OneLtArchive app.

--- src/main/java/lt/one/service/PostService.java
+++ src/main/java/lt/one/service/PostService.java
@@ -2,12 +2,10 @@ package lt.one.service;
 
 import lombok.Data;
 import lt.one.dto.PostResponseDTO;
-import lt.one.model.User;
 import lt.one.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Data
@@ -17,7 +15,7 @@ public class PostService {
 	private final PostMappingService postMappingService;
 
 	public List<PostResponseDTO> getAllUserPosts(final Long id) {
-		Optional<User> user = userRepository.findById(id);
-		return postMappingService.mapToResponseDTO(user.get().getPosts());
+		var user = userRepository.findById(id).get();
+		return postMappingService.mapToResponseDTO(user.getPosts());
 	}
 }
